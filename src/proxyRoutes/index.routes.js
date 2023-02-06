84387e6c6586d26a904dc0ccf7002e680cf240e9
Gustavo Silva <gustavost1y@gmail.com>
2023-02-06 10:17:22 -0300

    feat: new service added

--- src/proxyRoutes/index.routes.js
+++ src/proxyRoutes/index.routes.js
@@ -1,9 +1,12 @@
 const Router = require("express");
 const authServiceRoutes = require("./authService.routes");
 const crudService = require("./crudService.routes");
+const matchService = require("./matchService.routes");
+
 const router = Router();
 
 authServiceRoutes(router);
 crudService(router);
+matchService(router);
 
 module.exports = router;
---
+++ src/proxyRoutes/matchService.routes.js
@@ -0,0 +1,18 @@
+const httpProxy = require("express-http-proxy");
+const match = httpProxy("http://localhost:4000");
+
+const matchService = (router) => {
+  router.get("/match-response", (req, res, next) => {
+    match(req, res, next);
+  });
+
+  router.get("/get-animal", (req, res, next) => {
+    match(req, res, next);
+  });
+
+  router.post("pet-bus", (req, res, next) => {
+    match(req, res, next);
+  });
+};
+
+module.exports = matchService;
